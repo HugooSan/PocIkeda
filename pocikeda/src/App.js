58e3a742f34d370ada8af27acfd150f241e7f7de
Hugo Shinde <hugo.shinde@k2m.com.br>
2023-10-20 16:15:13 -0300

    Start React project

---
+++ pocikeda/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Cabecalho from './components/Cabecalho';
+
+function App() {
+  return (
+    <div className="App">
+      <Cabecalho />
+    </div>
+  );
+}
+
+export default App;
---
+++ pocikeda/src/components/Cabecalho/index.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import './cabecalho.css'
+
+function Cabecalho(props) {
+
+  return (
+    <header>
+        <Button>Home</Button>
+    </header>
+  );
+}
+
+
+export default Cabecalho;

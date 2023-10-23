0eb40a0b837f252cb981aaa1acdee37fa16f55c5
Hugo Shinde <hugo.shinde@k2m.com.br>
2023-10-23 17:24:47 -0300

    Banner

--- src/components/Cabecalho/index.js
+++ src/components/Cabecalho/index.js
@@ -1,19 +1,25 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
-import Typography from '@mui/material/Typography';
-import Link from '@mui/material/Link';
 import './cabecalho.css'
 
 function Cabecalho(props) {
 
   return (
-    <header>
-        <Button>Home</Button>
+
+
+    <header className='banner'>
+      <div className='logo'>
+        <img className='logotipo' alt='Logo da Empresa' src='https://github.com/HugooSan.png'/>
+      </div>
+      <div className='cabecalho'>
+        <ul className='navegacao'>
+          <li>
+            Caminho  
+          </li>
+        </ul>
+      </div>
     </header>
+
   );
 }
 

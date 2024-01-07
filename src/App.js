387776458b7d33ddfaa64d06d5dda631346af713
Hugo Shinde <hugo.shinde@k2m.com.br>
2024-01-07 15:08:52 -0300

    Add MUI components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Cabecalho from './components/Cabecalho';
+import Preco from './components/Preco';
 
 function App() {
   return (
     <div className="App">
       <Cabecalho />
+      <Preco />
     </div>
   );
 }
--- src/components/Cabecalho/index.js
+++ src/components/Cabecalho/index.js
@@ -1,24 +1,65 @@
 import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import './cabecalho.css'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import StarIcon from '@mui/icons-material/StarBorder';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import Container from '@mui/material/Container';
+
 
 function Cabecalho(props) {
 
   return (
 
 
-    <header className='banner'>
-      <div className='logo'>
-        <img className='logotipo' alt='Logo da Empresa' src='https://github.com/HugooSan.png'/>
-      </div>
-      <div className='cabecalho'>
-        <ul className='navegacao'>
-          <li>
-            Caminho  
-          </li>
-        </ul>
-      </div>
-    </header>
+    <AppBar
+      position="static"
+      color="default"
+      elevation={0}
+      sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
+    >
+      <Toolbar sx={{ flexWrap: 'wrap' }}>
+        <Typography variant="h4" color="inherit" noWrap sx={{ flexGrow: 1 }}>
+          Salão Ikeda
+        </Typography>
+        <nav>
+          <Link
+            variant="button"
+            color="text.primary"
+            href="#"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Sobre Nós
+          </Link>
+          <Link
+            variant="button"
+            color="text.primary"
+            href="#"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Valores
+          </Link>
+          <Link
+            variant="button"
+            color="text.primary"
+            href="#"
+            sx={{ my: 1, mx: 1.5 }}
+          >
+            Contato
+          </Link>
+        </nav>
+      </Toolbar>
+    </AppBar>
 
   );
 }
